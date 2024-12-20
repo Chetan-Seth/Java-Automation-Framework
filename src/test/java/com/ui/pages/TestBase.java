ab3fcdda389738b75e09bdd5d8b6354280fd6f4f
ankit <ankitqatechie@gmail.com>
2024-12-20 08:10:51 +0530

    testbase update

--- src/test/java/com/ui/pages/TestBase.java
+++ src/test/java/com/ui/pages/TestBase.java
@@ -1,5 +1,7 @@
 package com.ui.pages;
 
+import static com.constants.Browser.CHROME;
+
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
@@ -9,47 +11,52 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import com.constants.Browser;
+import com.ui.pages.HomePage;
 import com.utilities.BrowserUtility;
 import com.utilities.LambdaTestUtility;
 import com.utilities.LoggerUtility;
 
 public class TestBase {
 
-	public HomePage homePage; // creating instance variable to avoid the use of local variable
+	protected HomePage homePage;
 	Logger logger = LoggerUtility.getLogger(this.getClass());
 	private boolean isLambdaTest;
-	
-	
-	@Parameters({"browser","isLambdaTest","isHeadless"})
-	
-	@BeforeMethod(description = "Load the Home page of the Website")
-	public void setup(
-		@Optional("chrome") String browser , 
-		@Optional("false")boolean isLambdaTest,
-		@Optional("true")boolean isHeadless,ITestResult result) {
-		
-		this.isLambdaTest= isLambdaTest;
+
+
+	@Parameters({"browser", "isLambdaTest", "isHeadless"})
+	@BeforeMethod(description = "Load the Homepage of the Application")
+	public void setUp(
+			@Optional("chrome")String browser,
+			@Optional("false")boolean isLambdaTest,
+			@Optional("false")boolean isHeadless, ITestResult result) {
+
+		this.isLambdaTest = isLambdaTest;
+
 		WebDriver lambdaDriver;
 		if (isLambdaTest) {
+
 			lambdaDriver = LambdaTestUtility.initializeLambdaTestSession(browser, result.getMethod().getMethodName());
 			homePage = new HomePage(lambdaDriver);
 		} else {
-			logger.info("Load the Home page of the Website");
+			// running the test on local machine
+			logger.info("Load the Homepage of the Application");
 			homePage = new HomePage(Browser.valueOf(browser.toUpperCase()), isHeadless);
 		}
+
 	}
 
 	public BrowserUtility getInstance() {
+
 		return homePage;
 	}
 
-	@AfterMethod(description="Tear Down the Browser")
+	@AfterMethod(description = "Tear Down the Browser")
 	public void tearDown() {
 		if (isLambdaTest) {
-			LambdaTestUtility.quitSession();//quit browser session on LambdaTest
+			LambdaTestUtility.quitSession();// close the browser session on lambdatest
 		} else {
-			homePage.quit();//local 
+			homePage.quit(); // close the local machine session
 		}
-	}
 
+	}
 }
